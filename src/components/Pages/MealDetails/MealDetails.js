e24088062fa15680b6e106b10fa10773b423dfc3
Khalidprithy <khalidprithy@gmail.com>
2022-03-25 14:55:23 +0600

    order added

--- src/components/Pages/MealDetails/MealDetails.js
+++ src/components/Pages/MealDetails/MealDetails.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './MealDetails.css'
 
-const MealDetails = ({ ShowMealDetails }) => {
-    console.log(ShowMealDetails);
+const MealDetails = (props) => {
+    const { children } = props;
+    console.log(children);
     return (
         <div className='meal-details'>
-            <h1>Details</h1>
+            <div>
+                <img src={children[1].image} alt="" />
+                <p><strong>{children[1].title.slice(0, 16)}</strong></p>
+            </div>
+            <div>
+                <p>Time: {children[1].readyInMinutes}min</p>
+                <p>Serving: {children[1].servings}</p>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Meals/Meals.js
+++ src/components/Pages/Meals/Meals.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Meals.css'
 
-const Meals = ({ random }) => {
+const Meals = ({ random, ShowMealDetails }) => {
     // console.log(random);
     const { image, title, pricePerServing } = random;
     return (
-        <div className='meals'>
+        <div onClick={() => ShowMealDetails(random)} className='meals'>
             <img src={image} alt="" />
             <div className='meal-info'>
                 <h4>{title.slice(0, 40)}</h4>
